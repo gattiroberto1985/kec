fc0f3d1c22b419bd965a7d28f7540a52d7d3599d
Roberto Gatti <roberto.gatti@workpc.it>
2018-05-22 13:34:02 +0200

    Icon, readme and fixes in index.js

---
+++ README.md
@@ -0,0 +1,16 @@
+# Keep Electron Client
+
+This is a wrapper for google keep, based on electron. Useful for having a 
+dedicated window with your note manager, without searching the tab in 
+the browser!
+
+To launch:
+
+    npm install
+
+    npm start
+
+## Next step
+
+- Build system
+- Deploy system
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const {app, BrowserWindow} = require('electron')
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow( /* {width: 800, height: 600} */ )
+    win = new BrowserWindow( 
+        { icon: "./img/kec-icon.png" }
+        /* {width: 800, height: 600} */ 
+    )
 
     // and load the index.html of the app.
     win.loadURL( "https://keep.google.com" 
