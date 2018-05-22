2e5f7cf2a84ac99c64b17581603a067445b9858c
Roberto Gatti <roberto.gatti@workpc.it>
2018-05-22 15:24:27 +0200

    updated title, inserted shell electron for future usage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-const {app, BrowserWindow} = require('electron')
-//const path = require('path')
-//const url = require('url')
+const {app, BrowserWindow, shell} = require('electron');
+const path = require('path');
+const url = require('url');
+
 
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow( 
-        { icon: "./img/kec-icon.png" }
+        { 
+            icon:  "./img/kec-icon.png",
+            title: "KEC - Keep Electron Client"
+     }
         /* {width: 800, height: 600} */ 
     )
+
+    // Bypassing the title change event
+    win.on( "page-title-updated", function( event ) {
+        event.preventDefault();
+    });
 
     // and load the index.html of the app.
     win.loadURL( "https://keep.google.com" 
@@ -17,7 +26,12 @@ function createWindow () {
             slashes: true
         })
     */
-    )
+    );
+
+    //win.on("will-navigate", function( event ) {
+    //    console.log(" --> Navigating to '" + event.url + '" . . .');
+    //    //shell.openExternal(link);
+    //});
 }
 
 app.on('ready', createWindow);
